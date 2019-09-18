f1f4534d9a43b2534b2339f1650865b72b15c913
Yi Liu <47644936+ayumiryu@users.noreply.github.com>
2019-09-18 13:18:29 -0400

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -3,3 +3,17 @@
 
 	// if(document.getElementById('box').offsetWidth < "100px"){
 	// document.getElementById('box').style.width = "100%";}
+var db1 = document.getElementById('bdes1');
+var db2 = document.getElementById('bdes2');
+var db3 = document.getElementById('bdes3');
+
+if(document.body.clientWidth > 760){
+	db1.style.fontSize = "14px";
+	db1.style.margin = "20px";
+
+	db2.style.fontSize = "14px";
+	db2.style.margin = "20px";
+
+	db3.style.fontSize = "14px";
+	db3.style.margin = "20px";
+}
