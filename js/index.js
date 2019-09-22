84c6ab207b617e7d4682b9dd73f4e00d0ec7f0a1
Yi Liu <47644936+ayumiryu@users.noreply.github.com>
2019-09-21 23:18:39 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+ //document.getElementById("box").style.width="100px";
+
+
+	// if(document.getElementById('box').offsetWidth < "100px"){
+	// document.getElementById('box').style.width = "100%";}
+var db1 = document.getElementById('com_e');
+var db2 = document.getElementById('com_m');
+var db3 = document.getElementById('com_c1');
+var db4 = document.getElementById('com_c2');
+var db5 = document.getElementById('com_c3');
+var db6 = document.getElementById('com_c4');
+
+
+if(document.body.clientWidth > 768){
+	db1.style.fontSize = "16px";
+
+	db2.style.fontSize = "16px";
+
+	db3.style.fontSize = "16px";
+	db4.style.fontSize = "16px";
+	db5.style.fontSize = "16px";
+	db6.style.fontSize = "16px";
+}
